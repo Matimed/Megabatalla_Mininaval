9ef2c5fc9c65edc7599fa7f63de80e52771dda98
ArcosJuan <arcos0juan@gmail.com>
2021-07-14 00:30:20 -0300

    Completo el metodo inicializador y validar_posicion

--- Programa.py
+++ Programa.py
@@ -1,3 +1,4 @@
+from Jugador import Jugador
 from Posicion import Posicion
 from Tablero import Tablero
 import string
@@ -10,11 +11,20 @@ class Programa:
         for i in range(cant_filas): # La cantidad de columnas se define a partir de cant_filas
             [self.posiciones.append(Posicion(string.ascii_uppercase[i], x)) for x in range(cant_filas)]
 
-        self.tableros = [Tablero(self.posiciones), Tablero(self.posiciones)]
-        self.jugadores = []
+        cant_barcos = 8
+        self.tableros = [Tablero(self.posiciones, cant_barcos), Tablero(self.posiciones, cant_barcos)]
+        self.jugadores = [Jugador(self, self.tableros[0]), Jugador(self, self.tableros[1])]
 
     def validar_posicion(self, y, x):
         return (y, x) in self.posiciones
 
-    def traducir_posicion(int, char):
+    def traducir_posicion(self, y, x):
+        return self.posiciones.index((y, x))
+
+    def juego(self):
+        for jugador in self.jugadores:
+            jugador.preparar_tablero()
         
+        while True:
+            self.jugadores[0].disparar
+            self.jugadores[1].disparar
