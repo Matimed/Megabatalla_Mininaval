1d5a21acbd6adb5bedf17ff68632e04675d4cc39
ArcosJuan <arcos0juan@gmail.com>
2021-07-14 20:58:46 -0300

    actualizacion en inicializador y completos los  metodos set_nombre y ingresar_posicion

--- Jugador.py
+++ Jugador.py
@@ -1,30 +1,52 @@
-from Programa import Programa
+#from Programa import Programa
 from Tablero import Tablero
 
 class Jugador:
-    def __init__(self, tablero):
-        assert isinstance(tablero, Tablero), "El parametro tablero debe ser instacia de la clase Tablero"
-        assert type(nombre) == str, "El parametro nombre debe ser de tipo str" 
+    def __init__(self, tablero, programa):
+        assert isinstance(tablero, Tablero), "El argumento tablero debe ser instacia de la clase Tablero"
         
+        self.nombre = ''
         self.tablero = tablero
         self.mapa = {}
+        self.programa = programa
 
-    def preparar_tablero():
+    def set_nombre(self, nombre):
+        assert type(nombre) == str, "El argumento nombre debe ser de tipo str"
+        assert nombre, "El argumento nombre no puede estar vacio"
+
+        self.nombre = nombre
+
+
+    def ingresar_posicion(self):
+        "De acuerdo a la entrada del usuario devuelve una instancia de Posicion"
+
+        while True:
+            y = input("Ingrese la fila: ").strip().upper()
+            x = int(input("Ingrese la columna: ").strip())
+
+            if self.programa.validar_posicion(y, x): break
+
+            print("Esa posicion no existe, vuelva a intentar")
         
+        return self.programa.traducir_posicion(y, x)
+        
+    
+    def preparar_tablero():
+        return NotImplementedError
+
 
     def disparar():
-        return
+        return NotImplementedError
+
 
     def recibir_disparo(posicion):
-        return
+        return NotImplementedError
 
-    def ingresar_posicion(y, x):
-        return
-
+    
     def vaciar_tablero():
-        return 
+        return NotImplementedError
+
 
     def ubicacion_aleatoria():
-        return
-
+        return NotImplementedError
 
