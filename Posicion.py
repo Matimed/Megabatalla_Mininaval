257a07049c3a17aee8969de430a914223a5709b7
ArcosJuan <arcos0juan@gmail.com>
2021-07-13 23:22:07 -0300

    Se contemplan las comparaciones entre objetos dos objetos Posicion y entre un objeto Posicion y una tupla

--- Posicion.py
+++ Posicion.py
@@ -4,7 +4,7 @@ class Posicion:
     """
 
     def __init__(self, y, x):
-        self.verificar_formato(x, y)
+        self.verificar_formato(y, x)
 
         self.x = x
         self.y = y.upper()
@@ -14,21 +14,22 @@ class Posicion:
 
     def __eq__(self, other):
         assert (isinstance(other, Posicion) or type(other) == tuple), "No es posible comparar una Posicion con otro objeto"
-        assert len(other) == 2, "La tupla pasada por parametro debe constar de dos elementos"
-        self.verificar_formato(other[0], other[1])
-        
+
         if isinstance(other, Posicion):
+            self.verificar_formato(self.y, self.x)
+
             return other.get_posicion() == self.get_posicion()
         elif type(other) == tuple:
-            return (other[0], other[1].upper()) == self.get_posicion()
+            assert len(other) == 2, "La tupla pasada por parametro debe constar de dos elementos"
+            self.verificar_formato(other[0], other[1])
 
-    def verificar_formato(self, x, y):
+            return (other[0].upper(), other[1]) == self.get_posicion()
+
+
+    def verificar_formato(self, y, x):
         assert type(x) == int, "'x' debe ser int"
         assert type(y) == str, "'y' debe ser str"
         assert len(y) == 1, "'y' debe ser una cadena de un solo caracter"
 
     def __repr__(self):
         return f"({self.y}, {self.x})"
-
-
-print(Posicion('a', 3) == Posicion('a', 4))
