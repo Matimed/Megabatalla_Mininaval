39932ccc8eb47d58b67af17309564d50d3bf0df0
ArcosJuan <arcos0juan@gmail.com>
2021-07-14 00:31:34 -0300

    Clase Jugador creada solo con inicializador completo

---
+++ Jugador.py
@@ -0,0 +1,30 @@
+from Programa import Programa
+from Tablero import Tablero
+
+class Jugador:
+    def __init__(self, tablero):
+        assert isinstance(tablero, Tablero), "El parametro tablero debe ser instacia de la clase Tablero"
+        assert type(nombre) == str, "El parametro nombre debe ser de tipo str" 
+        
+        self.tablero = tablero
+        self.mapa = {}
+
+    def preparar_tablero():
+        
+
+    def disparar():
+        return
+
+    def recibir_disparo(posicion):
+        return
+
+    def ingresar_posicion(y, x):
+        return
+
+    def vaciar_tablero():
+        return 
+
+    def ubicacion_aleatoria():
+        return
+
+
