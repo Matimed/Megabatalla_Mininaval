60616d835c523839a0022f0936bc67ae61207c7b
ArcosJuan <arcos0juan@gmail.com>
2021-07-16 14:46:46 -0300

    POR FIN un implementacion de singledispatch para sobrecargar el metodo __eq__, gracias a https://stackoverflow.com/users/1126841/chepner

--- classes/posicion.py
+++ classes/posicion.py
@@ -1,3 +1,5 @@
+from functools import singledispatch, singledispatchmethod
+
 class Posicion:
     """ Sistema de coordenadas que utiliza la notacion 'x', 'y'
         para representar univocamente una ubicacion particular 
@@ -18,21 +20,27 @@ class Posicion:
         return self.y, self.x
 
 
+    @singledispatchmethod
     def __eq__(self, other):
-        assert (isinstance(other, Posicion) or type(other) == tuple), "No es posible comparar una Posicion con otro objeto"
+        try:
+            other_get_posicion = other.get_posicion
+        except AttributeError:
+            raise NotImplementedError()
 
-        if isinstance(other, Posicion):
-            self.verificar_formato(self.y, self.x)
+        return self.get_posicion() == other_get_posicion()
 
-            return other.get_posicion() == self.get_posicion()
-        elif type(other) == tuple:
-            assert len(other) == 2, "La tupla pasada por parametro debe constar de dos elementos"
-            self.verificar_formato(other[0], other[1])
 
-            return (other[0].upper(), other[1]) == self.get_posicion()
+    @__eq__.register(tuple)
+    def _(self, other):
+        assert len(other) == 2, "La tupla pasada por parámetro debe constar de dos elementos"
+        self.verificar_formato(other[0], other[1])
+
+        return (other[0].upper(), other[1]) == self.get_posicion()
+
 
     def __hash__(self):
         return hash((self.y, self.x))
+
 
     def verificar_formato(self, y, x):
         assert type(x) == int, "'x' debe ser int"
