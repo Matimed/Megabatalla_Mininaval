9dce8621c04c051ed4595d6e6ff14eea9598c2ae
ArcosJuan <arcos0juan@gmail.com>
2021-07-19 18:41:14 -0300

    Documentación añadida y corregida

--- classes/jugador.py
+++ classes/jugador.py
@@ -82,11 +82,11 @@ class Jugador:
     
 
     def agregar_barco(self):
-        """ Pide al jugador que ingrese una posición y 
-            llama al método de tablero agregar_barco con dicha posición.
+        """ Permite al jugador agregar un barco 
+            según la posicion que el ingrese.
         """
 
-        while True:
+        while True: # Hasta que se termine realizar la operación con exito.
             posicion = self.ingresar_posicion()
 
             try:
@@ -100,11 +100,11 @@ class Jugador:
 
 
     def quitar_barco(self):
-        """ Pide al jugador que ingrese una posición y 
-            llama al metodo de método quitar_barco con dicha posición.
+        """ Permite al jugador quitar un barco 
+            según la posicion que el ingrese.
         """
 
-        while True:
+        while True: # Hasta que se termine realizar la operación con exito.
             posicion = self.ingresar_posicion()
 
             try:
