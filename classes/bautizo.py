ac22c55e8e5c563f631c405eb886c90bc4caf1d4
Matimed11 <matiascarazomedley@gmail.com>
2021-08-07 20:12:38 -0300

    Clases Estado creadas (lptm)

--- classes/bautizo.py
+++
@@ -1,8 +0,0 @@
-from classes.estado import Estado
-
-class Bautizo(Estado):
-    """ Etapa donde se le asigna un nombre a cada uno de los jugadores.
-    """
-
-    def __init__(self):
-        super().__init__()
