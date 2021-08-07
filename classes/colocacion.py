b2cbe21ab1fc9528cd3330987df850f0990869c1
Matimed11 <matiascarazomedley@gmail.com>
2021-08-07 19:52:14 -0300

    Clases de Estado creadas

---
+++ classes/colocacion.py
@@ -0,0 +1,11 @@
+from classes.estado import Estado
+
+class Colocacion(Estado):
+    """ Etapa donde los jugadores distribuyen sus barcos
+        en posiciones determinadas de sus tableros
+        que termina cuando todos los barcos de ambos jugadores
+        fueron colocados de manera satisfactoria.
+    """
+
+    def __init__(self):
+        super().__init__()
