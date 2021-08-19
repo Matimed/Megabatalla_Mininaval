a3622b9acf43d5485c028068913184e2cad13ae5
ArcosJuan <arcos0juan@gmail.com>
2021-08-19 14:29:30 -0300

    EventosGenerales cambiado de Enum a IntEnum.

--- src/events/eventos_generales.py
+++ src/events/eventos_generales.py
@@ -1,12 +1,10 @@
-from enum import Enum
+from enum import IntEnum
 from pygame import event
 import pygame
 from pygame.constants import QUIT
 
-class EventosGenerales(Enum):
+class EventosGenerales(IntEnum):
     
     SALIR = pygame.QUIT
     CONFIGURADO = pygame.event.custom_type() # orden (int), cant_barcos (int)
-    
-
-
+    CAMBIAR_ESTADO = pygame.event.custom_type() # estado (Estado)
