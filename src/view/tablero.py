597fc5ee9573c7f42ee4a89f0f7a430d1727cd80
Matimed11 <mcarazomedley@gmail.com>
2021-09-12 03:10:28 -0300

    Metodos Update y Draw implementados y creacion de posiciones mejorada

--- src/view/tablero.py
+++ src/view/tablero.py
@@ -1,31 +1,83 @@
 from pygame.sprite import AbstractGroup
-from model import barco
 from view.sprites import SpriteCelda
 from view.sprites import SpriteBarco
 
 class TableroView(AbstractGroup):
-    def __init__(self):
-        self.celdas = {}
-        self.barcos = []
+    def __init__(self, cant_barcos, posiciones):
+        self.celdas = self._preparar_celdas(posiciones)
+        self.barcos = self._generar_barcos(cant_barcos)
 
-    
-    def generar_celdas(self, orden):
-        """ Recibe el orden del tablero y genera 
-            los SpritesCeldas necesarios para llenarlo."""
 
-        celdas = []
-        [celdas.append(SpriteCelda()) for i in range (orden**2)]
+    def update(self, pos_barcos, pos_celdas_marcadas = []):
+        """ Actualiza el estado de todos los sprites del tablero en
+            base a los argumentos dados y devuelve los barcos visibles. 
+
+            Recibe:
+                pos_barcos: Posicion[]    
+                pos_celdas_marcadas: Posicion[]
+
+            Devuelve:
+                barcos: SpriteBarco[]
+        """
+
+        barcos = self._ubicar_barcos(pos_barcos)
+        if pos_celdas_marcadas:
+            for barco in barcos:
+                barco.update(True)
+            for posicion in self.celdas.keys:
+                marcada = posicion in pos_celdas_marcadas
+                self.celdas[posicion].update(marcada)
+        else:
+            for barco in barcos:
+                barco.update(False)
+            for celda in self.celdas.values:
+                celda.update(False)
+        
+        return barcos
+
+
+    def draw(self, surface, barcos):
+        """ Recibe una superficie (Surface) y una lista de objetos SpriteBarco
+            y coloca en ella todos los sprites del tablero.
+        """
+
+        for celda in self.celdas.values():
+            celda.draw(surface)
+        for barco in barcos:
+            barco.draw(surface)
+
+
+    def _generar_barcos(self, cant_barcos):
+        """ Genera la cantidad pedida de objetos tipo SpriteBarco
+            y devuelve una lista con todos los objetos creados.
+        """
+
+        barcos = []
+        [barcos.append(SpriteBarco()) for i in range (cant_barcos)]
+        return barcos
+
+
+    def _preparar_celdas(self, posiciones):
+        """ Segun la lista de objetos tipo Posicion dada genera y ubica
+            una celda por cada posicion y devuelve dicha lista.
+        """
+        
+        celdas = self._generar_celdas(posiciones)
+        self._ubicar_celdas(posiciones, celdas)
+        self._asignar_posiciones_celdas(celdas)
         return celdas
 
 
-    def generar_barcos(self, cant_barcos):
-        """ Recibe la cantidad de barcos requerida y llena la lista de
-            barcos ocultos con esa cantidad de SpriteBarcos."""
+    def _generar_celdas(self, posiciones):
+        """ Recibe una lista de posiciones y genera 
+            un SpritesCelda por cada una."""
 
-        [self.barcos.append(SpriteBarco()) for i in range (cant_barcos)]
+        celdas = []
+        [celdas.append(SpriteCelda()) for i in range (len(posiciones))]
+        return celdas
 
 
-    def ubicar_celdas(self, posiciones, celdas, origen):
+    def _ubicar_celdas(self, posiciones, celdas, origen):
         """ Recorre todas las Posiciones y asigna un SpriteCelda
             a cada una de manera ordenada.
             
@@ -67,16 +119,16 @@ class TableroView(AbstractGroup):
                 celda_anterior = celda_actual
 
 
-    def asignar_posiciones_celdas(self):
-        """ Recorre el diccionario de celdas y le indica
+    def _asignar_posiciones_celdas(self, celdas):
+        """ Recorre un diccionario de celdas y le indica
             a cada una de ellas cual es su posición."""
 
-        for posicion in self.celdas:
-            celda = self.celdas[posicion]
+        for posicion in celdas:
+            celda = celdas[posicion]
             celda.set_posicion(posicion)
 
 
-    def ubicar_barcos(self, posiciones):
+    def _ubicar_barcos(self, posiciones):
         """ Recibe una lista de objetos de tipo Posicion que ubican
             todos los barcos que deben visualizarse en pantalla
             y devuelve una lista con todos los objetos tipo SpriteBarco
@@ -92,12 +144,6 @@ class TableroView(AbstractGroup):
             barco.ubicar(celda.get_rect().center)
             barcos_visibles.append(barco)
         
+
         return barcos_visibles
-
-
-
-
-
-
-                
-            
+        
