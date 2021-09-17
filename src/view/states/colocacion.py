289d70548779d1abb7793840bf8fc8a2ea5e3ed2
Matimed11 <mcarazomedley@gmail.com>
2021-09-17 06:29:20 -0300

    Tablero de colocacion creado.

--- src/view/states/colocacion.py
+++ src/view/states/colocacion.py
@@ -1,4 +1,5 @@
 from view.states import Estado
+from view.tablero import TableroView
 
 class Colocacion(Estado):
     """ Etapa donde los jugadores distribuyen sus barcos
@@ -7,5 +8,17 @@ class Colocacion(Estado):
         fueron colocados de manera satisfactoria.
     """
 
-    def __init__(self):
+    def __init__(self, tableros):
+        """ Recibe la referencia del tablero del modelo 
+            para poder comunicarse con él."""
+
         super().__init__()
+        self.modelo_tableros = tableros
+        self.vista_tablero = self._crear_tablero()
+
+
+    def _crear_tablero(self):
+        cant_barcos = self.modelo_tableros[0].cant_barcos
+        posiciones =  self.modelo_tableros[0].posiciones
+         
+        return TableroView(cant_barcos, posiciones, (0,0),(50,50))
