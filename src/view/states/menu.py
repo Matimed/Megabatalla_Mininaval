e24c6db2d99ba48c4298a83f693f4215728d1ecc
Matimed11 <mcarazomedley@gmail.com>
2021-09-17 04:34:38 -0300

    Nombres e indexacion corregidos en menú.

--- src/view/states/menu.py
+++ src/view/states/menu.py
@@ -16,12 +16,17 @@ class Menu(Estado):
     def actualizar(self, eventos):
         for sprite in self.sprites.values():
             if sprite.update():
-                if sprite == self.sprites['btJugar']:
-                    finalizar_estado = pygame.event.Event(evento_gb.ESTADO.valor, tipo=evento_estado.FINALIZAR_ESTADO, estado=Menu)
+                if sprite == self.sprites['bt_jugar']:
+                    finalizar_estado = pygame.event.Event(
+                                        evento_gb.ESTADO.valor, 
+                                        tipo=evento_estado.FINALIZAR_ESTADO, 
+                                        estado=Menu
+                                        )
+                                        
                     pygame.event.post(finalizar_estado)
 
 
-                if sprite == self.sprites['btSalir']:
+                if sprite == self.sprites['bt_salir']:
                     salir = pygame.event.Event(evento_gb.SALIR.valor)
                     pygame.event.post(salir)
             sprite.draw(Estado.ventana_sur)
@@ -34,9 +39,9 @@ class Menu(Estado):
             y devuelve un diccinario que los contiene.
         """
 
-        titulo = SpriteCajaTexto('Batalla      Naval', (0,0,0), 36)
-        btJugar = SpriteBotonTexto('Jugar', 70)
-        btSalir = SpriteBotonTexto('Salir', 70)
+        tx_titulo = SpriteCajaTexto('Batalla      Naval', (0,0,0), 36)
+        bt_jugar = SpriteBotonTexto('Jugar', 70)
+        bt_salir = SpriteBotonTexto('Salir', 70)
         
         centro_x = Estado.ventana.get_center()[0]
         centro_y = Estado.ventana.get_center()[1]
@@ -44,14 +49,14 @@ class Menu(Estado):
         # Posiciona los sprites de forma
         # relativa al tamaño de la ventana.
 
-        titulo.get_rect().center = (centro_x, centro_y - centro_y* 2/3 )
-        btJugar.get_rect().center = (centro_x, centro_y - centro_y* 1/6)
-        btSalir.get_rect().center = (centro_x, centro_y + centro_y* 1/3)
+        tx_titulo.get_rect().center = (centro_x, centro_y - centro_y* 2/3 )
+        bt_jugar.get_rect().center = (centro_x, centro_y - centro_y* 1/6)
+        bt_salir.get_rect().center = (centro_x, centro_y + centro_y* 1/3)
 
         sprites = {
-            'titulo' : titulo,
-            'btJugar' : btJugar,
-            'btSalir' : btSalir
+            'tx_titulo' : tx_titulo,
+            'bt_jugar' : bt_jugar,
+            'bt_salir' : bt_salir
         }
 
         return sprites
